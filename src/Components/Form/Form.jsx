0a031f475978d57230747097af6254a6a72906f5
Steve <scampbellatc@gmail.com>
2023-07-14 10:51:38 -0400

    started form logic

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import data from '../../memesData'
 
 export default function Form(){
+    const FindMeme = () => {
+        const randomMeme = data.data.memes
+        
+        const randomNum = Math.floor(Math.random() * randomMeme.length) 
+        console.log(randomNum)
+        // Below, create the logic to select a random meme using the above random number generator
+        // randomMeme.map(item => {
+
+        // })
+    }
+
+    
+    
+    
+    
     return(
         <main>
-            <form  className="form-container">
+            <div  className="form-container">
 
                 <input type="text" placeholder="Top text" className="form--input" />
                 <input type="text" placeholder="Bottom text" className="form--input" />
-                <button className="form--btn">Get a new meme image 🖼</button>
-            </form>
+                <button onClick={FindMeme} className="form--btn">Get a new meme image 🖼</button>
+            </div>
 
         </main>
 
