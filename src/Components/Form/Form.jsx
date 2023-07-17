9a04e0c25d7e586007fc5b9ca98c84112793ac55
Steve <scampbellatc@gmail.com>
2023-07-17 19:26:30 -0400

    updated memeData and styles

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,14 +1,44 @@
+
 import React from 'react'
+import data from '../../memesData'
+
+
+
+
 
 export default function Form(){
+    const [memeImage, setMemeImage] = React.useState('')
+
+   function getMemeImage() {
+        
+        const meme = data.data.memes
+        
+        const randomNum = Math.floor(Math.random() * meme.length) 
+
+        setMemeImage(meme[randomNum].url)
+        
+        }
+
+    
+
+    
+    
+    
+    
     return(
         <main>
-            <form  className="form-container">
+            <div  className="form-container">
 
                 <input type="text" placeholder="Top text" className="form--input" />
                 <input type="text" placeholder="Bottom text" className="form--input" />
-                <button className="form--btn">Get a new meme image 🖼</button>
-            </form>
+                <button onClick={getMemeImage} className="form--btn">Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+            <img className="meme-image" src={memeImage} />
+
+            </div>
+            
+                       
 
         </main>
 
