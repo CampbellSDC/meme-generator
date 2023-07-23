220f2a8fe6d53b7d7e8ef14e0a03d603cd2bc587
Steve <scampbellatc@gmail.com>
2023-07-23 18:40:46 -0400

    updated link inputs

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -35,17 +35,51 @@ export default function Form(){
 
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     
     return(
         <main>
             <div  className="form-container">
 
-                <input type="text" placeholder="Top text" className="form--input" />
-                <input type="text" placeholder="Bottom text" className="form--input" />
-                <button onClick={getMemeImage} className="form--btn">Get a new meme image 🖼</button>
+                <input 
+                type="text" 
+                placeholder="Top text" 
+                className="form--input"
+                name="topText"
+                value={meme.topText}
+                onChange={handleChange}
+                />
+
+                <input 
+                type="text" 
+                placeholder="Bottom text" 
+                className="form--input"
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}
+                />
+
+                <button 
+                    onClick={getMemeImage} 
+                    className="form--btn">
+
+                    Get a new meme image 🖼
+                    
+                </button>
+                
             </div>
             <div className="meme">
             <img className="meme-image" src={meme.randomImage} />
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
 
             </div>
             
